ae974085958a828d4997928340adab123c29150c
messenjer <mathieu@mirahi.io>
2022-09-14 15:00:18 +0200

    feat(init): setup @nuxtjs/tailwindcss

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="flex justify-center items-center h-screen">
+      <div class="grid gap-4 grid-cols-2 grid-rows-2">
+        <button
+          class="h-10 px-6 font-semibold rounded-md bg-black text-white"
+          type="submit"
+        >
+          Primary
+        </button>
+
+        <button
+          class="h-10 px-6 font-semibold rounded-md bg-white text-black border-solid border-black border-2"
+          type="submit"
+        >
+          Secondary
+        </button>
+      </div>
+    </div>
   </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
