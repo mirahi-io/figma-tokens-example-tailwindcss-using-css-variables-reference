05116c0e74bea6b9545f6ad446d761c70b5212b8
messenjer <mathieu@mirahi.io>
2022-09-14 17:12:15 +0200

    feat: 🎸 build css variable from figma-tokens Using Figma Token plugin export and StyleDictionary, we can auto generate the css-variable with their reference + the config override for tailwind (in separated files group by token type)

--- app.vue
+++ app.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <div class="flex justify-center items-center h-screen">
+    <div class="flex justify-center items-center h-screen bg-white">
       <div class="grid gap-4 grid-cols-2 grid-rows-2">
         <button
-          class="h-10 px-6 font-semibold rounded-md bg-black text-white"
+          class="h-10 px-6 font-semibold rounded-md bg-bg-default text-fg-default"
           type="submit"
         >
           Primary
         </button>
 
         <button
-          class="h-10 px-6 font-semibold rounded-md bg-white text-black border-solid border-black border-2"
+          class="h-10 px-6 font-semibold rounded-md bg-bg-muted text-fg-muted border-solid border-black border-2"
           type="submit"
         >
           Secondary
